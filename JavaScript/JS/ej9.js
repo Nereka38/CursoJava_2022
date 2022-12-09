48b4140fc7b1d562e978f1ff37274a61d7362871
Fernández Cuesta <nerea.fernandez-cuesta@capgemini.com>
2022-12-09 14:26:01 +0100

    add form js

--- JavaScript/JS/ej9.js
+++ JavaScript/JS/ej9.js
@@ -2,8 +2,33 @@ document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("form").addEventListener('submit', validarFormulario);
 });
 
+document.getElementById("descuento").addEventListener('click',function(){
+    alert('Esta es la mejor opción');
+})
+
 function validarFormulario(evento) {
     evento.preventDefault();
+
+    let selectProvincias = document.getElementById('provincias');
+    if (selectProvincias.selectedIndex == 0) {
+        alert("Debe seleccionar una provincia")
+    }
+
+    let dia = document.getElementById('dia').value;
+    let mes = document.getElementById('mes');
+    let año = document.getElementById('año').value;
+    if (dia == "" || dia == 0 || dia >= 31 || isNaN(dia)) {
+        alert("Añade un día correcto")
+    }
+
+    if (mes.selectedIndex == 0) {
+        alert("Selecciona un mes")
+    }
+
+    if (año == "" || año == 0 || año <= 1900 || isNaN(año)) {
+        alert("Añade un año correcto")
+    }
+
     let name = document.getElementById('name').value;
     if (name.length == 0) {
         alert('No has escrito nada en el nombre');
@@ -20,17 +45,12 @@ function validarFormulario(evento) {
         return;
     }
 
-    let select = document.getElementById('impuestos');
-    if (select.selectedIndex == 0) {
+    let selectImpuesto = document.getElementById('impuestos');
+    if (selectImpuesto.selectedIndex == 0) {
         alert("Debe seleccionar un impuesto")
     }
     if (select.selectedIndex == 1 || select.selectedIndex == 2) {
         alert("Debe seleccionar un impuesto mayor de 16%")
-    }
-
-    let descuento = document.getElementById('descuento').value;
-    if (descuento == descuento) {
-        alert("Esta opción es la más indicada")
     }
     this.submit();
 }
